244725350de880a0fb82e003316bf160f257bec6
Mihail Darin <pyh2007@yandex.ru>
2023-08-17 12:02:53 +0300

    UserData исправил пароль на 6 символов

--- src/main/java/user/UserData.java
+++ src/main/java/user/UserData.java
@@ -5,14 +5,14 @@ import org.apache.commons.lang3.RandomStringUtils;
 public class UserData {
     public static User getUserData(){
         final String email = "diplom_qa_java_21@yandex.ru";
-        final String password = "12345";
+        final String password = "123456";
         final String name = "Диплом";
         return new User(email, password, name);
     }
 
     public static User getUserDataWithoutEmail(){
         final String email = null;
-        final String password = "12345";
+        final String password = "123456";
         final String name = "Диплом";
         return new User(email, password, name);
     }
@@ -24,7 +24,7 @@ public class UserData {
     }
     public static User getUserDataWithoutName(){
         final String email = "diplom_qa_java_21@yandex.ru";
-        final String password = "12345";
+        final String password = "123456";
         final String name = null;
         return new User(email, password, name);
     }
@@ -38,13 +38,13 @@ public class UserData {
 
     public static User getUserDataLogin(){
         final String email = "diplom_qa_java_21@yandex.ru";
-        final String password = "12345";
+        final String password = "123456";
         return new User(email, password);
     }
 
     public static User getUserDataLoginWrongEmail(){
         final String email = RandomStringUtils.randomAlphabetic(10) + "@yandex.ru";
-        final String password = "12345";
+        final String password = "123456";
         return new User(email, password);
     }
 
